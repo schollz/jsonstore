e290543f587f1c7d718543af893eb057be4ec55b
Zack Scholl <zack.scholl@gmail.com>
2017-01-25 14:38:32 -0500

    First pass

---
+++ jsonstore.go
@@ -0,0 +1,136 @@
+package jsonstore
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"os"
+	"strings"
+	"sync"
+)
+
+type JSONStore struct {
+	Data     map[string]interface{}
+	location string
+	sync.RWMutex
+}
+
+func (s *JSONStore) Init() {
+	s.location = "data.json"
+	s.Data = make(map[string]interface{})
+}
+
+func (s *JSONStore) SetLocation(location string) {
+	s.Lock()
+	s.location = location
+	s.Unlock()
+}
+
+func (s *JSONStore) Load() error {
+	s.Lock()
+	defer s.Unlock()
+	if _, err := os.Stat(s.location); os.IsNotExist(err) {
+		return errors.New("Location does not exist")
+	} else {
+		b, err2 := ioutil.ReadFile(s.location)
+		if err != nil {
+			return err
+		}
+		err2 = json.Unmarshal(b, &s.Data)
+		return err2
+	}
+}
+
+func (s *JSONStore) Save() error {
+	s.Lock()
+	defer s.Unlock()
+	b, err := json.MarshalIndent(s.Data, "", " ")
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(s.location, b, 0644)
+	return err
+}
+
+func (s *JSONStore) Set(key string, value interface{}) error {
+	s.set(key, value)
+	s.Save()
+	return nil
+}
+
+func (s *JSONStore) SetMem(key string, value interface{}) error {
+	s.set(key, value)
+	return nil
+}
+
+func (s *JSONStore) set(key string, value interface{}) error {
+	s.Lock()
+	defer s.Unlock()
+	s.Data[key] = value
+	return nil
+}
+
+func (s *JSONStore) Get(key string) (interface{}, error) {
+	if strings.Contains(key, "*") {
+		return s.getmany(key)
+	} else {
+		return s.getone(key)
+	}
+}
+
+func (s *JSONStore) getmany(key string) (interface{}, error) {
+	s.RLock()
+	defer s.RUnlock()
+	possible := []string{}
+	for _, substring := range strings.Split(key, "*") {
+		if strings.Contains(substring, "*") || len(substring) == 0 {
+			continue
+		}
+		possible = append(possible, substring)
+	}
+
+	m := make(map[string]interface{})
+	for key := range s.Data {
+		for _, substring := range possible {
+			if strings.Contains(key, substring) {
+				m[key] = s.Data[key]
+			}
+		}
+	}
+
+	if len(m) == 0 {
+		return -1, errors.New(key + " not found")
+	}
+	return m, nil
+}
+
+func (s *JSONStore) getone(key string) (interface{}, error) {
+	s.RLock()
+	defer s.RUnlock()
+	val, ok := s.Data[key]
+	if !ok {
+		return -1, errors.New(key + " not found")
+	}
+	return val, nil
+}
+
+func main() {
+	var fs JSONStore
+	fs.Init()
+	fs.SetLocation("urls.json")
+	fs.Load()
+	fs.Set("12", "34")
+	// fs.Save()
+	//
+	// fs.Lock()
+	// fmt.Println(fs.Data)
+	// for key := range fs.Data {
+	// 	fmt.Println(key, fs.Data[key])
+	// }
+	// fmt.Println(fs.Data["j"])
+	// fs.Unlock()
+	// fs.Set("new", "test")
+	// fs.Set("new", []string{"ab", "asd", "asdf"})
+	// fs.Save()
+	// fs.Load()
+}
---
+++ jsonstore_test.go
@@ -0,0 +1,91 @@
+package jsonstore
+
+import (
+	"os"
+	"testing"
+)
+
+func BenchmarkGet(b *testing.B) {
+	var fs JSONStore
+	fs.Init()
+	fs.Set("data", 1234)
+	fs.Set("name", "bob")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		fs.Get("data")
+	}
+}
+
+func BenchmarkGetMany(b *testing.B) {
+	var fs JSONStore
+	fs.Init()
+	fs.Set("name:1", "bob")
+	fs.Set("name:2", "zack")
+	fs.Set("name:3", "bill")
+	fs.Set("country:1", "usa")
+	fs.Set("country:2", "usa")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		fs.Get("name*")
+	}
+}
+
+func BenchmarkSet(b *testing.B) {
+	var fs JSONStore
+	fs.Init()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		fs.Set("data", 1234)
+	}
+}
+
+func BenchmarkSetMem(b *testing.B) {
+	var fs JSONStore
+	fs.Init()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		fs.Set("data", 1234)
+	}
+}
+
+func TestGetMany(t *testing.T) {
+	var fs JSONStore
+	fs.Init()
+	fs.Set("name:1", "bob")
+	fs.Set("name:2", "zack")
+	fs.Set("name:3", "bill")
+	fs.Set("country:1", "usa")
+	fs.Set("country:2", "usa")
+	val, err := fs.Get("name*")
+	if err != nil {
+		t.Errorf("Got %+v, and %s", val, err.Error())
+	}
+}
+
+func TestSet(t *testing.T) {
+	// Test simple saving and getting
+	var fs JSONStore
+	fs.Init()
+	fs.Set("name", "zack")
+
+	var fs2 JSONStore
+	fs2.Init()
+	fs2.Load()
+	val, err := fs2.Get("name")
+	if err != nil || val != "zack" {
+		t.Errorf("Got %+v, and %s", val, err.Error())
+	}
+
+	// Test saving a different place
+	os.Remove("test.json")
+	fs.SetLocation("test.json")
+	fs.SetMem("name2", "zack2") // doesn't persist
+	fs.Save()                   // now its saved
+
+	fs2.SetLocation("test.json")
+	fs2.Load()
+	val, err = fs2.Get("name2")
+	if err != nil || val != "zack2" {
+		t.Errorf("Got %+v, and %s", val, err.Error())
+	}
+}
